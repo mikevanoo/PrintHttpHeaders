4f075d3a40f2c6b120b8d10298f0fb3f77a3532f
Mike van Oorschot <mike.vanoorschot@focus-solutions.co.uk>
2022-06-24 13:46:44 +0100

    Initial commit.

---
+++ PrintHttpHeaders/Program.cs
@@ -0,0 +1,31 @@
+namespace PrintHttpHeaders
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", async context =>
+            {
+                // request headers
+                await context.Response.WriteAsync("REQUEST HEADERS:\n");
+                foreach (var requestHeader in context.Request.Headers)
+                {
+                    await context.Response.WriteAsync($"{requestHeader.Key} = {requestHeader.Value}\n");
+                }
+                await context.Response.WriteAsync($"RemoteIpAddress = {context.Connection.RemoteIpAddress}");
+
+                // response headers
+                await context.Response.WriteAsync("\n\nRESPONSE HEADERS:\n");
+                foreach (var responseHeader in context.Response.Headers)
+                {
+                    await context.Response.WriteAsync($"{responseHeader.Key} = {responseHeader.Value}\n");
+                }
+            });
+            
+            app.Run();
+        }
+    }
+}
