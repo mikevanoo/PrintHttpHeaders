25253c6ae645dd4601c12e306f49dd2f88422f37
Mike van Oorschot <michael.oorschot@codurance.com>
2022-08-25 15:06:50 +0100

    Changed to minimal api.

--- PrintHttpHeaders/Program.cs
+++ PrintHttpHeaders/Program.cs
@@ -1,31 +1,22 @@
-namespace PrintHttpHeaders
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", async context =>
 {
-    public class Program
+    // request headers
+    await context.Response.WriteAsync("REQUEST HEADERS:\n");
+    foreach (var requestHeader in context.Request.Headers)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
-
-            app.MapGet("/", async context =>
-            {
-                // request headers
-                await context.Response.WriteAsync("REQUEST HEADERS:\n");
-                foreach (var requestHeader in context.Request.Headers)
-                {
-                    await context.Response.WriteAsync($"{requestHeader.Key} = {requestHeader.Value}\n");
-                }
-                await context.Response.WriteAsync($"RemoteIpAddress = {context.Connection.RemoteIpAddress}");
-
-                // response headers
-                await context.Response.WriteAsync("\n\nRESPONSE HEADERS:\n");
-                foreach (var responseHeader in context.Response.Headers)
-                {
-                    await context.Response.WriteAsync($"{responseHeader.Key} = {responseHeader.Value}\n");
-                }
-            });
-            
-            app.Run();
-        }
+        await context.Response.WriteAsync($"{requestHeader.Key} = {requestHeader.Value}\n");
     }
-}
+    await context.Response.WriteAsync($"RemoteIpAddress = {context.Connection.RemoteIpAddress}");
+
+    // response headers
+    await context.Response.WriteAsync("\n\nRESPONSE HEADERS:\n");
+    foreach (var responseHeader in context.Response.Headers)
+    {
+        await context.Response.WriteAsync($"{responseHeader.Key} = {responseHeader.Value}\n");
+    }
+});
+            
+app.Run();
